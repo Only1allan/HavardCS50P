89e3e73d18361ed6670986734bc1fc7db490c2b0
Only1allan <kariukiallan850@gmail.com>
2023-07-01 13:36:41 +0300

    added camel question

--- Loops/camel/camel.py
+++ Loops/camel/camel.py
@@ -1,3 +1,10 @@
+"""
+In a file called camel.py, implement a program that prompts the user for the name of a 
+variable in camel case and outputs the corresponding name in snake case.
+Assume that the user’s input will indeed be in camel case.
+
+"""
+
 def main():
     name = input("camelCase: ")
     snake = snakecase(name)
